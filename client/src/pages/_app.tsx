89f295dbb927d3eb94d159ce069b9cc9a9cf5550
kisho@aegon-linuxMachine <kisokumar886@gmail.com>
2023-08-18 09:52:28 +0100

    feat: use caching with urql

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,73 @@
-import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { Cache, QueryInput, cacheExchange } from "@urql/exchange-graphcache";
+import { Client, Provider, fetchExchange } from "urql";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import theme from "../theme";
 
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
 const client = new Client({
   url: "http://localhost:4000/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, _args, cache, _info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, _args, cache, _info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
   fetchOptions: {
     credentials: "include",
   },
